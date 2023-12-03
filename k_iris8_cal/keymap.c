09d65081ee576665a869ad4ecdbdb34aa8287577
tmp <wsl@dein0s-torrent>
2023-12-03 17:13:16 +0100

    iris: callum WIP fix typo

--- k_iris8_cal/keymap.c
+++ k_iris8_cal/keymap.c
@@ -167,7 +167,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _FN1, _FN2, _FN3);
+    return update_tri_layer_state(state, _RGT, _LFT, _ADJ);
 }
 
 #ifdef ENCODER_MAP_ENABLE
